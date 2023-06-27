e3103d6bf9a1e5bcd3de3f4313fb7c1a9c088a3d
cybernO0b <92986665+cybernO0b@users.noreply.github.com>
2023-06-27 22:37:34 +0500

    Смена порядка нумерации сегментов
    Изменен порядок нумерации, но инфа о бое теперь не сохраняется.

--- Details/gumps/janela_principal.lua
+++ Details/gumps/janela_principal.lua
@@ -6061,15 +6061,15 @@ local build_segment_list = function(self, elapsed)
 		local dungeon_run_id = false
 
 		--> history table(segments container)
-		for i = _detalhes.segments_amount, 1, -1 do
-
-			if(i <= fill) then
-
-				local thisCombat = _detalhes.tabela_historico.tabelas[i]
-				if(thisCombat) then
+		for i = 1, _detalhes.segments_amount do
+			local reversed_i = _detalhes.segments_amount - i + 1
+		
+			if (reversed_i <= fill) then
+				local thisCombat = _detalhes.tabela_historico.tabelas[reversed_i]
+				if (thisCombat) then
 					local enemy = thisCombat.is_boss and thisCombat.is_boss.name
 					local segment_info_added = false
-
+		
 					segments_used = segments_used + 1
 
 					--print(thisCombat.is_boss.name, thisCombat.instance_type, _detalhes:GetRaidIcon(thisCombat.is_boss.mapid), thisCombat.is_boss.ej_instance_id)
